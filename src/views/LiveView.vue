d31d1ac0cffd5b17c589fbd2a8f75d57b720db02
jackyr <jackyrao2009@gmail.com>
2023-04-18 20:51:10 +0800

    feat: up

--- src/views/LiveView.vue
+++ src/views/LiveView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { ref, computed, watch, onMounted } from 'vue'
-  import { useRoute } from 'vue-router'
+  import { useRoute, useRouter } from 'vue-router'
   import LiveControlMask from '@/components/LiveControlMask.vue'
   import { useAppData } from '@/stores/appData'
   import { getLiveChannel, type ChannelData } from '@/services/liveService'
@@ -10,8 +10,16 @@
   import { ErrorMsg } from '@/common/config'
 
   type ChannelName = '' | 'livetv' | 'amtb' | 'sanshi' | 'wdmaster' | 'education' | 'English'
+  const channelDescMap = {
+    livetv: '淨空\r\n老和尚',
+    wdmaster: '悟道\r\n法師',
+    education: '多元\r\n文化',
+    sanshi: '三時\r\n繫念',
+    amtb: '網路\r\n念佛',
+  }
 
   const route = useRoute()
+  const router = useRouter()
   const currentChannelName = ref<ChannelName>(route.params.channel as ChannelName || 'sanshi')
   const currentPlaylistItem = ref<ChannelData["playlist"][number]>()
   const videoRef = ref<HTMLVideoElement>()
@@ -108,6 +116,16 @@
       </div>
     </section>
   </div>
+  <section class="navBar">
+    <a
+      :class="['item', { current: v === live?.name }]"
+      v-for="(v, i) in Object.keys(channelDescMap)"
+      :key="i"
+      @click="router.replace(`/live/${v}`)"
+    >
+      {{ channelDescMap[v as keyof typeof channelDescMap] }}
+    </a>
+  </section>
 </template>
 
 <style scoped lang="less">
@@ -117,6 +135,7 @@
   background-color: #F1EAE6;
   display: flex;
   flex-direction: column;
+  padding-bottom: 64px;
 }
 .videoElmContainer {
   width: 100vw;
@@ -170,5 +189,39 @@
       margin-top: 17px;
     }
   }
+}
+.navBar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px 15px;
+  background: linear-gradient(180deg, rgba(255,255,255,0.9) 0%, #FFFFFF 100%);
+  box-shadow: 0px -4px 10px 0px rgba(0,0,0,0.03);
+  backdrop-filter: blur(10px);
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
+
+  .item {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 6px 0;
+    font-size: 12px;
+    font-weight: 600;
+    color: #987B67;
+    line-height: 15px;
+    background: rgba(255,255,255,0.1);
+    border-radius: 4px;
+    border: 1px solid rgba(152,123,103,0.1);
+    white-space: pre;
+    &.current {
+      background: #987B67;
+      color: #fff;
+    }
+  } 
 }
 </style>
